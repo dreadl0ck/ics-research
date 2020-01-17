70db53092fed1544203c46094eb5fdf170ada660
dreadl0ck <dreadl0ck@protonmail.ch>
2020-01-17 01:10:32 +0100

    extract-unique-ips: loop over pcap files in current dir

--- extract-unique-ips.sh
+++ extract-unique-ips.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-tcpdump -r *.pcap -nn -q ip -l | \
-    awk '{match($3,/[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/); \
-    ip = substr($3,RSTART,RLENGTH); \
-    if (!seen[ip]++) print ip }'
+for f in *.pcap
+do
+	tcpdump -r "$f" -nn -q ip -l | awk '{match($3,/[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/); ip = substr($3,RSTART,RLENGTH); if (!seen[ip]++) print ip }'
+done
+
 
