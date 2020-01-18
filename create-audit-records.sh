2b673c6f853a8286f13dc41950a7273b4025b225
dreadl0ck <dreadl0ck@protonmail.ch>
2020-01-18 14:23:41 +0100

    added script to generate audit records for all pcap in current dir

---
+++ create-audit-records.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+for f in *.pcap
+do
+	filename=$(basename -- "$f")
+	file=${filename%.pcap}
+	net.capture -r "$f" -out "/home/***REMOVED***/2019-SWaT-auditrecords/$file" -opts datagrams
+done
