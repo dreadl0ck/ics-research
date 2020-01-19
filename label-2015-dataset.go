b495a6caff7719ad1262c15fb219694aa690b465
Philipp Mieden <dreadl0ck@protonmail.ch>
2020-01-19 23:17:18 +0100

    improved tool output

--- label-2015-dataset.go
+++ label-2015-dataset.go
@@ -53,7 +53,7 @@ var header = []string{
 var (
 	// commandline flags
 	flagAttackList = flag.String("attacks", "", "attack list CSV")
-	flagDir        = flag.String("dir", ".", "input directory (default is current directory)")
+	flagInput        = flag.String("in", ".", "input directory (default is current directory)")
 	flagOut        = flag.String("out", ".", "output path")
 	flagNumWorkers = flag.Int("workers", 10, "number of parallel processed files")
 
@@ -92,7 +92,7 @@ func main() {
 		fmt.Println("done in", time.Since(start))
 	}()
 
-	err := filepath.Walk(*flagDir, func(path string, info os.FileInfo, err error) error {
+	err := filepath.Walk(*flagInput, func(path string, info os.FileInfo, err error) error {
 
 		if filepath.Ext(path) == ".csv" && !strings.HasSuffix(path, "-labeled.csv") {
 			files = append(files, path)
@@ -108,6 +108,8 @@ func main() {
 	fmt.Println("collected", totalFiles, "CSV files for labeling, num workers:", *flagNumWorkers)
 
 	fmt.Println("new CSV header:", header)
+	fmt.Println("output directory:", *flagOut)
+	fmt.Println("initializing", *flagNumWorkers, "workers")
 
 	for i:=0; i < *flagNumWorkers; i++ {
 		workers = append(workers, worker())
@@ -119,7 +121,7 @@ func main() {
 			file:       file,
 			current:    current,
 			totalFiles: totalFiles,
-			wg:         wg,
+			wg:         &wg,
 		})
 	}
 
@@ -139,136 +141,6 @@ func main() {
 // ClearLine clears the current line of the terminal
 func clearLine() {
 	print("\033[2K\r")
-}
-
-func label(file string, current, totalFiles int, wg sync.WaitGroup) {
-
-	var (
-		fileName   = file
-		currentNum = current + 1
-	)
-
-	fmt.Println("[", currentNum, "/", totalFiles, "]", "processing", fileName)
-
-	inputFile, err := os.Open(fileName)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer inputFile.Close()
-
-	var outFileName = strings.TrimSuffix(fileName, ".csv") + "-labeled.csv"
-	if *flagOut != "." {
-		outFileName = filepath.Join(*flagOut, outFileName)
-	}
-
-	outputFile, err := os.Create(outFileName)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer outputFile.Close()
-
-	var (
-		inputReader       = csv.NewReader(inputFile)
-		outputWriter      = csv.NewWriter(outputFile)
-		numMatches, count int
-	)
-
-	// write header
-	err = outputWriter.Write(header)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	for {
-		r, err := inputReader.Read()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			fmt.Println(err)
-			count++
-			continue
-		}
-		count++
-
-		// skip header
-		if count == 1 {
-			continue
-		}
-
-		// fields Network CSV:
-		// 0  num
-		// 1  date
-		// 2  time
-		// 3  orig
-		// 4  type
-		// 5  i/f_name
-		// 6  i/f_dir
-		// 7  src
-		// 8  dst
-		// 9  proto
-		// 10 appi_name
-		// 11 proxy_src_ip
-		// 12 Modbus_Function_Code
-		// 13 Modbus_Function_Description
-		// 14 Modbus_Transaction_ID
-		// 15 SCADA_Tag
-		// 16 Modbus_Value
-		// 17 service
-		// 18 s_port
-		// 19 Tag
-		// num,date,time,orig,type,i/f_name,i/f_dir,src,dst,proto,appi_name,proxy_src_ip,Modbus_Function_Code,Modbus_Function_Description,Modbus_Transaction_ID,SCADA_Tag,Modbus_Value,service,s_port,Tag
-		// 1,21Dec2015,22:17:56,192.168.1.48,log,eth1,outbound,192.168.1.60,192.168.1.10,tcp,CIP_read_tag_service,192.168.1.60,76,Read Tag Service,30721,HMI_LIT101,Number of Elements: 1,44818,53260,0
-
-		var classification = "normal"
-
-		t, err := time.Parse("2Jan200615:04:05", r[1]+r[2])
-		if err != nil {
-			t, err = time.Parse("2Jan0615:04:05", r[1]+r[2])
-			if err != nil {
-				t, err = time.Parse("2-Jan-0615:04:05", r[1]+r[2])
-				if err != nil {
-					log.Println("[", currentNum, "/", totalFiles, "]", err)
-					sec, err := strconv.ParseInt(r[1]+r[2], 10, 64)
-					if err != nil {
-						log.Fatal("[", currentNum, "/", totalFiles, "]", " no valid timestamp format found!")
-					}
-					t = time.Unix(sec, 0)
-				}
-			}
-		}
-		// fmt.Println(r[1]+r[2], "time:", t)
-
-		for _, a := range attacks {
-			if a.during(t) {
-				if a.affectsHosts(r[7], r[8]) {
-					classification = a.AttackName
-					fmt.Println("match for", a.AttackName)
-
-					hitMapLock.Lock()
-					hitMap[a.AttackName]++
-					hitMapLock.Unlock()
-
-					numMatches++
-					break
-				}
-			}
-		}
-
-		err = outputWriter.Write(append(r, classification))
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
-
-	outputWriter.Flush()
-	err = outputWriter.Error()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("[", currentNum+1, "/", totalFiles, "]", count, "records written to", outputFile.Name(), numMatches, "number of attacks labeled")
-	wg.Done()
 }
 
 /*
@@ -406,7 +278,7 @@ func parseAttackList(path string) (labels []*attack) {
 type task struct {
 	file string
 	current, totalFiles int
-	wg sync.WaitGroup
+	wg *sync.WaitGroup
 }
 
 func handleTask(t task) {
@@ -441,7 +313,7 @@ func worker() (chan task) {
 		for {
 			select {
 			case t := <-chanInput:
-				label(t.file, t.current, t.totalFiles, t.wg)
+				t.label()
 				continue
 			}
 		}
@@ -449,4 +321,131 @@ func worker() (chan task) {
 
 	// return input channel
 	return chanInput
+}
+
+func (t task) label() {
+
+	info := "["+ strconv.Itoa(t.current+1) + "/" + strconv.Itoa(t.totalFiles)+"]"
+	fmt.Println(info, "processing", t.file)
+
+	inputFile, err := os.Open(t.file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer inputFile.Close()
+
+	var outFileName = strings.TrimSuffix(t.file, ".csv") + "-labeled.csv"
+	if *flagOut != "." {
+		outFileName = filepath.Join(*flagOut, outFileName)
+	}
+
+	outputFile, err := os.Create(outFileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer outputFile.Close()
+
+	var (
+		inputReader       = csv.NewReader(inputFile)
+		outputWriter      = csv.NewWriter(outputFile)
+		numMatches, count int
+	)
+
+	// write header
+	err = outputWriter.Write(header)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		r, err := inputReader.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			fmt.Println("error while reading next line from file", t.file, "error:", err)
+			count++
+			continue
+		}
+		count++
+
+		// skip header
+		if count == 1 {
+			continue
+		}
+
+		// fields Network CSV:
+		// 0  num
+		// 1  date
+		// 2  time
+		// 3  orig
+		// 4  type
+		// 5  i/f_name
+		// 6  i/f_dir
+		// 7  src
+		// 8  dst
+		// 9  proto
+		// 10 appi_name
+		// 11 proxy_src_ip
+		// 12 Modbus_Function_Code
+		// 13 Modbus_Function_Description
+		// 14 Modbus_Transaction_ID
+		// 15 SCADA_Tag
+		// 16 Modbus_Value
+		// 17 service
+		// 18 s_port
+		// 19 Tag
+		// num,date,time,orig,type,i/f_name,i/f_dir,src,dst,proto,appi_name,proxy_src_ip,Modbus_Function_Code,Modbus_Function_Description,Modbus_Transaction_ID,SCADA_Tag,Modbus_Value,service,s_port,Tag
+		// 1,21Dec2015,22:17:56,192.168.1.48,log,eth1,outbound,192.168.1.60,192.168.1.10,tcp,CIP_read_tag_service,192.168.1.60,76,Read Tag Service,30721,HMI_LIT101,Number of Elements: 1,44818,53260,0
+
+		var classification = "normal"
+
+		ti, err := time.Parse("2Jan200615:04:05", r[1]+r[2])
+		if err != nil {
+			ti, err = time.Parse("2Jan0615:04:05", r[1]+r[2])
+			if err != nil {
+				ti, err = time.Parse("2-Jan-0615:04:05", r[1]+r[2])
+				if err != nil {
+					log.Println(info, err)
+					sec, err := strconv.ParseInt(r[1]+r[2], 10, 64)
+					if err != nil {
+						fmt.Println(info, " no valid timestamp format found!", t.file)
+						continue
+					}
+					ti = time.Unix(sec, 0)
+				}
+			}
+		}
+		// fmt.Println(r[1]+r[2], "time:", t)
+
+		for _, a := range attacks {
+			if a.during(ti) {
+				if a.affectsHosts(r[7], r[8]) {
+					classification = a.AttackName
+					//fmt.Println("match for", a.AttackName)
+
+					hitMapLock.Lock()
+					hitMap[a.AttackName]++
+					hitMapLock.Unlock()
+
+					numMatches++
+					break
+				}
+			}
+		}
+
+		err = outputWriter.Write(append(r, classification))
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	outputWriter.Flush()
+	err = outputWriter.Error()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(info, count, "records,", numMatches, "attacks written to", filepath.Base(outputFile.Name()))
+	t.wg.Done()
 }
