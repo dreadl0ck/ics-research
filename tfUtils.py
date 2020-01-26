d459d261dbdbf74be7730a37f749d685e88fed3b
Philipp Mieden <dreadl0ck@protonmail.ch>
2020-01-26 23:55:47 +0100

    added LSTM layer implementation to multi file reading code and improved logging

--- tfUtils.py
+++ tfUtils.py
@@ -5,8 +5,20 @@ import numpy as np
 import pandas as pd
 from termcolor import colored
 from sklearn import preprocessing
+from keras import layers
+from keras.layers import Dropout
 
-
+# metrics for model
+METRICS = [
+    keras.metrics.TruePositives(name='tp'),
+    keras.metrics.FalsePositives(name='fp'),
+    keras.metrics.TrueNegatives(name='tn'),
+    keras.metrics.FalseNegatives(name='fn'),
+    keras.metrics.BinaryAccuracy(name='accuracy'),
+    keras.metrics.Precision(name='precision'),
+    keras.metrics.Recall(name='recall'),
+    keras.metrics.AUC(name='auc'),
+]
 
 def encode_string(df, name):
     """
@@ -42,6 +54,155 @@ def encode_numeric_zscore(df, name, mean=None, sd=None):
     df[name] = (df[name] - mean) / sd
 
 encoders = {
+
+    # Flow / Connection
+    'TimestampFirst'   : encode_numeric_zscore,
+    'LinkProto'        : encode_string,
+    'NetworkProto'     : encode_string,
+    'TransportProto'   : encode_string,
+    'ApplicationProto' : encode_string,
+    'SrcMAC'           : encode_string,
+    'DstMAC'           : encode_string,
+    'SrcIP'            : encode_string,
+    'SrcPort'          : encode_numeric_zscore,
+    'DstIP'            : encode_string,
+    'DstPort'          : encode_numeric_zscore,
+    'Size'             : encode_numeric_zscore,
+    'AppPayloadSize'   : encode_numeric_zscore,
+    'NumPackets'       : encode_numeric_zscore,
+    'UID'              : encode_string,
+    'Duration'         : encode_numeric_zscore,
+    'TimestampLast'    : encode_numeric_zscore,
+
+    # UDP specific fields
+    'Length'           : encode_numeric_zscore,
+    'Checksum'         : encode_numeric_zscore,
+    'PayloadEntropy'   : encode_numeric_zscore,
+    'PayloadSize'      : encode_numeric_zscore,
+    'Timestamp'        : encode_numeric_zscore,
+
+    # TCP specific fields
+    'SeqNum'           : encode_numeric_zscore,
+    'AckNum'           : encode_numeric_zscore,
+    'DataOffset'       : encode_numeric_zscore,
+    'FIN'              : encode_bool,
+    'SYN'              : encode_bool,
+    'RST'              : encode_bool,
+    'PSH'              : encode_bool,
+    'ACK'              : encode_bool,
+    'URG'              : encode_bool,
+    'ECE'              : encode_bool,
+    'CWR'              : encode_bool,
+    'NS'               : encode_bool,
+    'Window'           : encode_numeric_zscore,
+    'Urgent'           : encode_numeric_zscore,
+    'Padding'          : encode_numeric_zscore,
+    'Options'          : encode_string,
+
+    # ARP
+    'AddrType'          : encode_numeric_zscore,
+    'Protocol'          : encode_numeric_zscore,
+    'HwAddressSize'     : encode_numeric_zscore,
+    'ProtAddressSize'   : encode_numeric_zscore,
+    'Operation'         : encode_numeric_zscore,
+    'SrcHwAddress'      : encode_string,
+    'SrcProtAddress'    : encode_string,
+    'DstHwAddress'      : encode_string,
+    'DstProtAddress'    : encode_string,
+
+    # Layer Flows
+    'Proto'                : encode_string,
+
+    # NTP
+    'LeapIndicator'        : encode_numeric_zscore,     #int32
+    'Version'              : encode_numeric_zscore,     #int32
+    'Mode'                 : encode_numeric_zscore,     #int32
+    'Stratum'              : encode_numeric_zscore,     #int32
+    'Poll'                 : encode_numeric_zscore,     #int32
+    'Precision'            : encode_numeric_zscore,     #int32
+    'RootDelay'            : encode_numeric_zscore,     #uint32
+    'RootDispersion'       : encode_numeric_zscore,     #uint32
+    'ReferenceID'          : encode_numeric_zscore,     #uint32
+    'ReferenceTimestamp'   : encode_numeric_zscore,     #uint64
+    'OriginTimestamp'      : encode_numeric_zscore,     #uint64
+    'ReceiveTimestamp'     : encode_numeric_zscore,     #uint64
+    'TransmitTimestamp'    : encode_numeric_zscore,     #uint64
+    'ExtensionBytes'       : encode_string,         #[]byte
+
+    # Ethernet
+    'EthernetType'        : encode_numeric_zscore,     #int32
+
+    # IPv4
+    'IHL'                : encode_numeric_zscore,  # int32
+    'TOS'                : encode_numeric_zscore,  # int32
+    'Id'                 : encode_numeric_zscore,  # int32
+    'Flags'              : encode_numeric_zscore,  # int32
+    'FragOffset'         : encode_numeric_zscore,  # int32
+    'TTL'                : encode_numeric_zscore,  # int32
+
+    # IPv6
+    'TrafficClass'     : encode_numeric_zscore,  # int32
+    'FlowLabel'        : encode_numeric_zscore,  # uint32
+    'Length'           : encode_numeric_zscore,  # int32
+    'NextHeader'       : encode_numeric_zscore,  # int32
+    'HopLimit'         : encode_numeric_zscore,  # int32
+    'SrcIP'            : encode_string,      # string
+    'DstIP'            : encode_string,      # string
+    'PayloadEntropy'   : encode_numeric_zscore,  # float64
+    'PayloadSize'      : encode_numeric_zscore,  # int32
+    'HopByHop'         : encode_string,      # *IPv6HopByHop
+
+    # HTTP
+    'Method'           : encode_string,
+    'Host'             : encode_string,
+    'UserAgent'        : encode_string,
+    'Referer'          : encode_string,
+    "ReqCookies"       : encode_string,
+    'ReqContentLength' : encode_numeric_zscore,
+    'URL'              : encode_string,
+    'ResContentLength' : encode_numeric_zscore,
+    'ContentType'      : encode_string,
+    'StatusCode'       : encode_numeric_zscore,
+
+    # DNS
+    'ID'           : encode_numeric_zscore, # int32
+    'QR'           : encode_bool, # bool
+    'OpCode'       : encode_numeric_zscore, # int32
+    'AA'           : encode_bool, # bool
+    'TC'           : encode_bool, # bool
+    'RD'           : encode_bool, # bool
+    'RA'           : encode_bool, # bool
+    'Z'            : encode_numeric_zscore, # int32
+    'ResponseCode' : encode_numeric_zscore, # int32
+    'QDCount'      : encode_numeric_zscore, # int32
+    'ANCount'      : encode_numeric_zscore, # int32
+    'NSCount'      : encode_numeric_zscore, # int32
+    'ARCount'      : encode_numeric_zscore, # int32
+    'Questions'    : encode_string,
+    'Answers'      : encode_string,
+    'Authorities'  : encode_string,
+    'Additionals'  : encode_string,
+
+    'Type'               : encode_numeric_zscore, # int32
+    'MessageLen'         : encode_numeric_zscore, # int32
+    'HandshakeType'      : encode_numeric_zscore, # int32
+    'HandshakeLen'       : encode_numeric_zscore, # uint32
+    'HandshakeVersion'   : encode_numeric_zscore, # int32
+    'Random'             : encode_string, # string
+    'SessionIDLen'       : encode_numeric_zscore,  # uint32
+    'SessionID'          : encode_string, # string, will be dropped
+    'CipherSuiteLen'     : encode_numeric_zscore,  # int32
+    'ExtensionLen'       : encode_numeric_zscore,  # int32
+    'SNI'                : encode_string, # string
+    'OSCP'               : encode_bool,   # bool
+    'CipherSuites'       : encode_string, # string
+    'CompressMethods'    : encode_string, # string
+    'SignatureAlgs'      : encode_string, # string
+    'SupportedGroups'    : encode_string, # string
+    'SupportedPoints'    : encode_string, # string
+    'ALPNs'              : encode_string, # string
+    'Ja3'                : encode_string, # string
+
     # SWaT 2015 Network CSVs
     "num"                          : encode_numeric_zscore,
     "date"                         : encode_string,
@@ -162,7 +323,7 @@ def file_size(file_path):
         print("not a file:", file_path)
 
 
-def process_dataset(df, sample, drop):
+def process_dataset(df, sample, drop, lstm):
     if sample != None:
         if sample >= 1.0:
             parser.error("invalid sample rate")
@@ -176,6 +337,17 @@ def process_dataset(df, sample, drop):
     if drop != None:
         for col in drop.split(","): 
             drop_col(col, df)
+
+    # Always drop columns that are unique for every record
+    drop_col('UID', df)
+
+    if not lstm:
+        drop_col('Timestamp', df)
+
+    drop_col('SessionID', df)
+    drop_col('num', df)
+    drop_col('date', df)
+    drop_col('time', df)
 
     print("[INFO] columns:", df.columns)
    
@@ -203,7 +375,7 @@ def analyze(df):
             print("[INFO] ** {}:{}".format(col,expand_categories(df[col])))
             expand_categories(df[col])
 
-def encode_columns(df, result_column):
+def encode_columns(df, result_column, lstm):
     for col in df.columns:
         colName = col.strip()
         if colName != result_column:
@@ -222,35 +394,73 @@ def encode_columns(df, result_column):
     print("[INFO] num_classes", num_classes)
     
     # Remove incomplete records after encoding
-    df.dropna(inplace=True,axis=1) 
+    df.dropna(inplace=True, axis=1)
+
+    if lstm:
+        # drop last elem from dataframe, in case it contains an uneven number of elements
+        if len(df) % 2 != 0:
+            print("odd number of items, dropping last one...")
+            df = df.iloc[:-1]
 
 
-def create_dnn(input_dim, output_dim, loss, optimizer):
+def create_dnn(input_dim, output_dim, loss, optimizer, lstm, numCoreLayers, dropoutLayer):
+
     # Create neural network
     # Type Sequential is a linear stack of layers
     model = Sequential()
     
-    # add layers
-    # first layer has to specify the input dimension
-    model.add(Dense(10, input_dim=input_dim, kernel_initializer='normal', activation='relu')) # OUTPUT size: 10
-    model.add(Dense(50, input_dim=input_dim, kernel_initializer='normal', activation='relu')) # OUTPUT size: 50
-    model.add(Dense(10, input_dim=input_dim, kernel_initializer='normal', activation='relu')) # OUTPUT size: 10
-    model.add(Dense(1, kernel_initializer='normal'))
-    model.add(Dense(output_dim,activation='softmax'))
-    
-    
-    METRICS = [
-        keras.metrics.TruePositives(name='tp'),
-        keras.metrics.FalsePositives(name='fp'),
-        keras.metrics.TrueNegatives(name='tn'),
-        keras.metrics.FalseNegatives(name='fn'),
-        keras.metrics.BinaryAccuracy(name='accuracy'),
-        keras.metrics.Precision(name='precision'),
-        keras.metrics.Recall(name='recall'),
-        keras.metrics.AUC(name='auc'),
-    ]
+    if lstm:
+
+        # construct input shape
+        input_shape=(1000,input_dim,)
+        print("> input_shape", input_shape)
+
+        lstmNeurons=12
+        print("> LSTM first and last layer neurons:", lstmNeurons)
+
+        # - The input of the LSTM is always a 3D array. (batch_size, time_steps, seq_len)
+        # - The output of the LSTM could be a 2D array or 3D array depending upon the return_sequences argument.
+        # - If return_sequence is False, the output is a 2D array. (batch_size, units)
+        # - If return_sequence is True, the output is a 3D array. (batch_size, time_steps, units)
+
+        model.add(layers.LSTM(lstmNeurons, input_shape=input_shape, return_sequences=True))
+
+        # add requested number of core layers
+        for i in range(0, numCoreLayers):
+            print("adding core layer", i)
+            model.add(layers.LSTM(24, input_shape=input_shape, return_sequences=True))
+
+        # add final LSTM layer
+        model.add(layers.LSTM(lstmNeurons, input_shape=input_shape, return_sequences=True))
+
+        # add dropout layer if requested
+        if dropoutLayer:
+            model.add(Dropout(rate=0.5))
+
+        # flatten if requested
+        # TODO: currently this breaks the shape
+        #model.add(Flatten())
+
+        # final layer
+        model.add(layers.Dense(output_dim, activation='softmax'))
+    else:
+
+        print(colored("[INFO] using sequential dense layers", 'yellow'))
+        # DNN
+        # add layers
+        # first layer has to specify the input dimension
+        model.add(Dense(25, input_dim=input_dim, kernel_initializer='normal', activation='relu')) # OUTPUT size: 10
+        model.add(Dense(100, input_dim=input_dim, kernel_initializer='normal', activation='relu')) # OUTPUT size: 50
+        model.add(Dense(25, input_dim=input_dim, kernel_initializer='normal', activation='relu')) # OUTPUT size: 10
+        model.add(Dense(1, kernel_initializer='normal'))
+        model.add(Dense(output_dim, activation='softmax'))
     
     # compile model
-    # 
-    model.compile(loss=loss, optimizer=optimizer, metrics=METRICS)
+    #
+    model.compile(
+        loss=loss, 
+        optimizer=optimizer, 
+        metrics=METRICS
+    )
+
     return model
