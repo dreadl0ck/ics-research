3e6c427ef90a8d3181b39be8c2f4b62c9b47ac41
Philipp Mieden <dreadl0ck@protonmail.ch>
2020-01-28 14:10:54 +0100

    patch for dropping cols

--- tfUtils.py
+++ tfUtils.py
@@ -293,7 +293,8 @@ def drop_col(name, df):
     """
     if name in df.columns:
         print(colored("dropping column: " + name, "yellow"))
-        df.drop(name, axis=1, inplace=True)
+        #df.drop(name, axis=1, inplace=True)
+        del df[name]
 
 ## File Size Utils
 
