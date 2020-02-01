3c0262ac5c5640db52d7abb61d1881d164df4c4c
Philipp Mieden <dreadl0ck@protonmail.ch>
2020-02-01 19:22:30 +0100

    updated exp

--- experiments/dnn-v3.sh
+++ experiments/dnn-v3.sh
@@ -1,24 +1,22 @@
 #!/bin/bash
 
-# SGD optimizer, dropout layer, corelayer: 10, wrap: 5, 20 epochs
+# dropout layer, no sgd, corelayer: 50, wrap: 10, 50 epochs
 
 python3 train.py \
     -read "data/SWaT2015-Attack-Files-v0.2/train/*-labeled.csv" \
-    -wrapLayerSize 5 \
+    -wrapLayerSize 10 \
     -dropoutLayer true \
-    -coreLayerSize 10 \
-    -epoch 20 \
+    -coreLayerSize 50 \
+    -epoch 50 \
     -features 15 \
-    -drop modbus_value \
-    -optimizer sgd
+    -drop modbus_value
 
 # EVAL
 python3 score.py \
     -read "data/SWaT2015-Attack-Files-v0.2/eval/*-labeled.csv" \
-    -wrapLayerSize 5 \
+    -wrapLayerSize 10 \
     -dropoutLayer true \
-    -coreLayerSize 10 \
-    -optimizer sgd \
+    -coreLayerSize 50 \
     -features 15 \
-    -weights checkpoints/dn-epoch-20-files-49-50 \
+    -weights checkpoints/dn-epoch-50-files-49-50 \
     -drop modbus_value
--- experiments/dnn-v5.sh
+++ experiments/dnn-v5.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# no sgd, dropoutlayer, corelayer: 50, wrap: 10, 20 epochs
+# NO dropout layer, corelayer: 50, wrap: 10, 50 epochs
 
 python3 train.py \
     -read "data/SWaT2015-Attack-Files-v0.2/train/*-labeled.csv" \
     -wrapLayerSize 10 \
-    -dropoutLayer true \
+    -optimizer sgd \
     -coreLayerSize 50 \
-    -epoch 20 \
+    -epoch 50 \
     -features 15 \
     -drop modbus_value
 
@@ -15,8 +15,8 @@ python3 train.py \
 python3 score.py \
     -read "data/SWaT2015-Attack-Files-v0.2/eval/*-labeled.csv" \
     -wrapLayerSize 10 \
-    -dropoutLayer true \
+    -optimizer sgd \
     -coreLayerSize 50 \
     -features 15 \
-    -weights checkpoints/dn-epoch-20-files-49-50 \
+    -weights checkpoints/dn-epoch-50-files-49-50 \
     -drop modbus_value
