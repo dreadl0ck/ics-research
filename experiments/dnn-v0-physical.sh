60c7a9ce30ef72246cb9051f8a1023e61aefa87d
rutger <rutger@bastia.studlab.os3.nl>
2020-02-03 16:11:14 +0100

    fix for physical data

--- experiments/dnn-v0-physical.sh
+++ experiments/dnn-v0-physical.sh
@@ -2,25 +2,27 @@
 
 # SGD optimizer, dropout layer, corelayer: 10, wrap: 5, 20 epochs
 
-python3 train.py \
-    -read "data/SWAT_Dataset_Attack_v0-fixed.csv" \
-    -wrapLayerSize 5 \
+python3 -u train.py \
+    -read "data/SWaT_Dataset_Attack_v0-fixed-zscore-train-test.csv" \
+    -wrapLayerSize 10 \
     -dropoutLayer true \
-    -coreLayerSize 10 \
+    -coreLayerSize 50 \
     -epoch 10\
     -features 45 \
     -drop modbus_value \
-    -resultColumn "Normal/Attack" \ 
-    -classes Normal,Attack
+    -resultColumn "Normal/Attack" -classes Normal,Attack 
 
 # EVAL
-python3 score.py \
-    -read "data/SWaT2015-Attack-Files-v0.2/train/2015-12-28_113021_98.log.part13_sorted-labeled.csv" \
-    -wrapLayerSize 5 \
+python3 -u score.py \
+    -read "data/SWaT_Dataset_Attack_v0-fixed-zscore-eval.csv" \
+    -wrapLayerSize 10 \
     -dropoutLayer true \
-    -coreLayerSize 10 \
-    -features 15 \
+    -coreLayerSize 50 \
+    -features 45 \
     -drop modbus_value \
     -resultColumn "Normal/Attack" \
-    -classes Normal,Attack
+    -classes Normal,Attack \
+    -batchSize 500
+
+
 
