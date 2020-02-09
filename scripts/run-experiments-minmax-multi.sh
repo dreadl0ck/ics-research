f66c131cfb269678d5f41c555bbb90dea2c44674
Philipp Mieden <dreadl0ck@protonmail.ch>
2020-02-09 12:59:44 +0100

    bump version in generated fiels

--- scripts/run-experiments-minmax-multi.sh
+++ scripts/run-experiments-minmax-multi.sh
@@ -2,6 +2,7 @@
 
 chmod +x experiments/*.sh
 
+# TODO: generate better output file names, and includ version number of python script automatically
 for f in experiments/*
 do
     rm -rf checkpoints/
@@ -9,5 +10,5 @@ do
     filename=$(basename -- "$f")
 	file=${filename%.sh}
     echo "running $f"
-    "./$f" "data/SWaT2015-Attack-Files-v0.4.3-minmax-text/train/*-labeled.csv" "data/SWaT2015-Attack-Files-v0.4.3-minmax-text/eval/*-labeled.csv" &> "experiment-logs/v0.4.3-multi-$file-minmax-text.log"
+    "./$f" "data/SWaT2015-Attack-Files-v0.4.3-minmax-text/train/*-labeled.csv" "data/SWaT2015-Attack-Files-v0.4.3-minmax-text/eval/*-labeled.csv" &> "experiment-logs/v0.4.5-binary-multi-$file-minmax-text.log"
 done
--- scripts/run-experiments-minmax-single.sh
+++ scripts/run-experiments-minmax-single.sh
@@ -2,6 +2,7 @@
 
 chmod +x experiments/*.sh
 
+# TODO: generate better output file names, and includ version number of python script automatically
 for f in experiments/*
 do
     rm -rf checkpoints/
@@ -9,5 +10,5 @@ do
     filename=$(basename -- "$f")
 	file=${filename%.sh}
     echo "running $f"
-    "./$f" "data/SWaT2015-Attack-Files-v0.4.3-minmax-text/train/2015-12-28_113021_98.log.part12_sorted-labeled.csv" "data/SWaT2015-Attack-Files-v0.4.3-minmax-text/train/2015-12-28_113021_98.log.part13_sorted-labeled.csv" &> "experiment-logs/v0.4.3-single-$file-minmax-text.log"
+    "./$f" "data/SWaT2015-Attack-Files-v0.4.3-minmax-text/train/2015-12-28_113021_98.log.part12_sorted-labeled.csv" "data/SWaT2015-Attack-Files-v0.4.3-minmax-text/train/2015-12-28_113021_98.log.part13_sorted-labeled.csv" &> "experiment-logs/v0.4.5-binary-single-$file-minmax-text.log"
 done
