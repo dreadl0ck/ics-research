179cd1b9d67af0c542c18ed85eca657405218d1c
Philipp Mieden <dreadl0ck@protonmail.ch>
2020-02-09 12:52:14 +0100

    logs update

--- scripts/fetch-experiment-logs.sh
+++ scripts/fetch-experiment-logs.sh
@@ -2,7 +2,7 @@
 
 echo "[INFO] downloading logs from ***REMOVED***"
 mkdir -p experiment-logs/***REMOVED***
-scp -P 9876 ***REMOVED***@***REMOVED***:/home/***REMOVED***/ics-research/experiment-logs/*.log experiment-logs/***REMOVED***
+scp -P 9876 ***REMOVED***@***REMOVED***:/ics/ics-research/experiment-logs/*.log experiment-logs/***REMOVED***
 
 echo "[INFO] downloading logs from brussels"
 mkdir -p experiment-logs/brussels
